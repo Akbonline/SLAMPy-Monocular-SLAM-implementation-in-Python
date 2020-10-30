640aaf6e87cb0228e8a49ff15835fcccecf7d0f6
“Akbonline” <akshatbajpai.biz@gmail.com>
2020-10-30 00:26:06 -0700

    adjusting the video properties

--- slam.py
+++ slam.py
@@ -2,6 +2,19 @@ import numpy as np
 import cv2
 import os,sys,time,g2o
 
+
+def calibrate():
+    # camera intrinsics
+    W, H = 1920//2, 1080//2
+    K = np.array([[F,0,W//2],[0,F,H//2],[0,0,1]])
+    Kinv = np.linalg.inv(K)
+    image = cv2.resize(image, (W,H))
+    return image
+
+def generate_SLAM(image):
+    image = calibrate(image)
+
+
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("%s takes in .mp4 as an arg" %sys.argv[0])
